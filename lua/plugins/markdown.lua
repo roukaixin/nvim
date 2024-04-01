46a808cfc4cf186522c57801f4887338d61c8c51
pankx <a3427173515@163.com>
2024-04-01 22:47:05 +0800

    refactor: 取消掉 markdown 插件

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,21 +1,20 @@
-return {
-  "iamcco/markdown-preview.nvim",
-  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-  build = "cd app && yarn install",
-  init = function()
-    vim.g.mkdp_filetypes = { "markdown" }
-  end,
-  keys = {
-    -- ctrl + s
-    { "<C-s>", "<cmd>MarkdownPreview<cr>", mode = "n", desc = "打开预览" },
-    -- alt + s
-    { "<M-s>", "<cmd>MarkdownPreviewStop<cr>", mode = "n", desc = "关闭预览"}
-  },
-  config = function()
-    -- 使用 wyeb 打开预览文件
-    vim.g.mkdp_browser = 'wyeb'
-    vim.g.mkdp_filetypes = {'markdown'}
-  end,
-  -- 加载特定的文件
-  ft = "markdown"
-}
+--return {
+--  "iamcco/markdown-preview.nvim",
+--  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+--  build = function()
+--    vim.fn["mkdp#util#install"]()
+--  end,
+--  keys = {
+--    -- ctrl + s
+--    { "<C-s>", "<cmd>MarkdownPreview<cr>", mode = "n", desc = "打开预览" },
+--    -- alt + s
+--    { "<M-s>", "<cmd>MarkdownPreviewStop<cr>", mode = "n", desc = "关闭预览"}
+--  },
+--  config = function()
+--    -- 使用 wyeb 打开预览文件
+--    vim.g.mkdp_browser = 'vimb'
+--    vim.g.mkdp_filetypes = {'markdown'}
+--  end,
+--  -- 加载特定的文件
+--  ft = "markdown"
+--}
