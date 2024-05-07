50f2320135087e4448d01885dab9af87b04f8c39
pankx <a3427173515@163.com>
2024-05-07 10:27:40 +0800

    feat: 添加主题插件

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
 require("core.options")
 require("core.keymaps")
 require("core.lazy")
+vim.cmd("colorscheme habamax")
---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,11 @@
+return {
+    {
+        "ntk148v/habamax.nvim",
+        lazy = true
+    },
+    {
+        "folke/tokyonight.nvim",
+        lazy = true,
+        opts = { style = "moon" }
+    }
+}
