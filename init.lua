eb0bea072bc24ad9432dfd05765c5646246c7192
pankx <a3427173515@163.com>
2024-05-07 10:48:31 +0800

    style: 使用 habamax 主题

--- init.lua
+++ init.lua
@@ -2,4 +2,4 @@ require("core.options")
 require("core.keymaps")
 require("core.lazy")
 vim.o.background = "dark" -- or "light" for light mode
-vim.cmd([[colorscheme dracula]])
+vim.cmd([[colorscheme habamax]])
