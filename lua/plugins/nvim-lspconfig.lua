301062cfe755078cddd04896455d9fe385763baf
pankx <a3427173515@163.com>
2024-05-07 16:37:32 +0800

    feat: 优化较少代码量

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,3 +1,4 @@
+-- lsp
 return {
     "neovim/nvim-lspconfig",
     dependencies = {
@@ -5,12 +6,32 @@ return {
         "williamboman/mason-lspconfig.nvim"
     },
     config = function()
-        require("mason").setup()
-        require("mason-lspconfig").setup()
         local lsp_config = require('lspconfig')
         local capabilities = require('cmp_nvim_lsp').default_capabilities()
-        lsp_config.clangd.setup({
-            capabilities = capabilities
+        local servers = {
+            clangd = {},
+            rust_analyzer = {}
+        }
+
+        local on_attach = function()
+
+        end
+
+        require("mason").setup()
+        require("mason-lspconfig").setup({
+            ensure_installed = vim.tbl_keys(servers),
         })
+
+        for server, config in pairs(servers) do
+            lsp_config[server].setup({
+                vim.tbl_deep_extend("keep",
+                        {
+                            on_attach = on_attach,
+                            capabilities = capabilities
+                        },
+                        config
+                )
+            })
+        end
     end
 }
