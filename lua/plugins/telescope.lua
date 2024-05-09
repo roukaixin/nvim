fc1aba3f1911d667534909746996821d7cb6a06d
pankx <a3427173515@163.com>
2024-05-09 15:44:10 +0800

    refactor: 添加快捷键配置

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,6 +7,12 @@ return {
             build = 'make'
         }
     },
+    keys = {
+        { '<leader>ff', "<cmd>lua require('telescope.builtin').find_files()<cr>", desc = "列出当前工作目录中的文件，尊重 .gitignore" },
+        { '<leader>fg', "<cmd>lua require('telescope.builtin').live_grep()<cr>",  desc = "在当前工作目录中搜索字符串并在键入时实时获取结果，尊重 .gitignore。 （需要 ripgrep）" },
+        { '<leader>fb', "<cmd>lua require('telescope.builtin').buffers()<cr>", desc = "列出当前 neovim 实例中打开的缓冲区" },
+        { '<leader>fh', "<cmd>lua require('telescope.builtin').help_tags()<cr>", desc = "列出可用的帮助标签并打开一个新窗口" },
+    },
     config = function()
         require('telescope').setup {
             extensions = {
