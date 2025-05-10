120c534b3e59d4509bfd4222ea6e6b3d3c113df3
roukaixin <a3427173515@163.com>
2025-05-10 22:45:12 +0800

    chore: 重写配置

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,38 +1,21 @@
 -- 格式化插件
 return {
 	"stevearc/conform.nvim",
-	config = function()
-		local registry = require("mason-registry")
-
-		local function install(name)
-			local success, package = pcall(registry.get_package, name)
-			if success and not package:is_installed() then
-				package:install()
-			end
-		end
-
-		local format_servers = {
-			"stylua",
-		}
-
-		for _, value in ipairs(format_servers) do
-			install(value)
-		end
-
-		require("conform").setup({
-			formatters_by_ft = {
-				lua = { "stylua" },
-				rust = { "rustfmt" },
-			},
-		})
-	end,
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			rust = { "rustfmt" },
+			yaml = { "yamlfmt" },
+		},
+	},
 	keys = {
 		{
 			"<leader>lf",
 			function()
 				require("conform").format()
 			end,
-			desc = "格式化",
+			mode = { "n", "v" },
+			desc = "format code",
 			silent = true,
 		},
 	},
