30769e5fc0f2623692d68267df0647df967aff52
roukaixin <a3427173515@163.com>
2025-05-10 22:42:59 +0800

    feat: 添加保存快捷键

--- lua/core/keymap.lua
+++ lua/core/keymap.lua
@@ -1,9 +1,10 @@
 vim.g.mapleader = " "
 
-vim.keymap.set({"n","i"}, "<C-z>", "<Cmd>undo<CR>", { silent = true })
+vim.keymap.set({ "n", "i" }, "<C-z>", "<Cmd>undo<CR>", { silent = true })
+vim.keymap.set({ "n", "i" }, "<C-s>", "<Cmd>write<CR>", { silent = true })
 
 -- ------------------- 插入模式 ------------------
 -- 把 esc 按键映射为 jk
-keymap.set("i", "jk", "<ESC>")
+vim.keymap.set("i", "jk", "<ESC>")
 
 -- -----------------------------------------------
