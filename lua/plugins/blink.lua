8f45b8f8f9b76c6a9dc59362ddc5e18c571bd577
roukaixin <a3427173515@163.com>
2025-05-10 22:43:49 +0800

    feat: 补全插件快捷键配置

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,7 +1,7 @@
 -- 补全
 return {
 	"saghen/blink.cmp",
-	version = "*",
+	version = "1.*",
 	dependencies = {
 		"rafamadriz/friendly-snippets",
 	},
@@ -9,20 +9,74 @@ return {
 	opts = {
 		completion = {
 			documentation = {
-				auto_show = true,
+				auto_show = false,
+				window = {
+					border = "rounded",
+				},
 			},
 			list = {
 				selection = {
-					preselect = false,
+					preselect = true,
 					auto_insert = true,
 				},
 			},
+			menu = {
+				border = "rounded",
+				draw = {
+					padding = { 1, 1 },
+					components = {
+						kind_icon = {
+							text = function(ctx)
+								return " " .. ctx.kind_icon .. ctx.icon_gap
+							end,
+						},
+						kind = {
+							text = function(ctx)
+								return ctx.kind
+							end,
+						},
+					},
+					columns = {
+						{ "label", "label_description", gap = 1 },
+						{ "kind_icon", "kind", gap = 1 },
+					},
+				},
+			},
+		},
+		signature = {
+			enabled = true,
+			window = {
+				border = "rounded",
+			},
 		},
 		keymap = {
-			preset = "enter",
+			preset = "none",
+			["<C-h>"] = { "show", "show_documentation", "hide_documentation" },
+			["<C-e>"] = { "hide", "fallback" },
+			["<CR>"] = { "accept", "fallback" },
+			["<Tab>"] = { "snippet_forward", "fallback" },
+			["<S-Tab>"] = { "snippet_backward", "fallback" },
+			["<Up>"] = { "select_prev", "fallback" },
+			["<Down>"] = { "select_next", "fallback" },
+			["<C-p>"] = { "select_prev", "fallback_to_mappings" },
+			["<C-n>"] = { "select_next", "fallback_to_mappings" },
+			["<C-b>"] = { "scroll_documentation_up", "fallback" },
+			["<C-f>"] = { "scroll_documentation_down", "fallback" },
+			["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
 		},
 		sources = {
-			default = { "path", "snippets", "buffer", "lsp" },
+			default = function(_)
+				local success, node = pcall(vim.treesitter.get_node)
+				if
+					success
+					and node
+					and vim.tbl_contains({ "comment", "line_comment", "block_comment" }, node:type())
+				then
+					return { "buffer" }
+				else
+					return { "lsp", "path", "snippets", "buffer" }
+				end
+			end,
 		},
 		cmdline = {
 			enabled = true,
@@ -32,7 +86,7 @@ return {
 				},
 				list = {
 					selection = {
-						preselect = false,
+						preselect = true,
 						auto_insert = true,
 					},
 				},
