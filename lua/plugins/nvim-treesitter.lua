6fa972c93f514f552f582ec47cd8d0b6c72373fa
roukaixin <a3427173515@163.com>
2025-05-10 22:39:26 +0800

    chore: 配置自动下载

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,8 +1,10 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	main = "nvim-treesitter.configs",
+    build = ":TSUpdate",
 	event = "VeryLazy",
 	opts = {
+        auto_install = true,
 		ensure_installed = { "lua" },
 		highlight = { enable = true },
 	},
