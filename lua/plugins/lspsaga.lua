4625aa2b39d1bc6e28c645afaa85792a0a145611
roukaixin <a3427173515@163.com>
2025-05-10 22:42:19 +0800

    feat: 添加 lspsaga 插件

---
+++ lua/plugins/lspsaga.lua
@@ -0,0 +1,29 @@
+-- 文档地址：https://nvimdev.github.io/lspsaga/
+-- :Lspsaga outline 代码大纲。命令：toggle_or_jump(o)/jump(e):跳转到代码位置、quit(q):退出
+return {
+	"nvimdev/lspsaga.nvim",
+	event = "LspAttach",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+		"nvim-tree/nvim-web-devicons",
+	},
+	opts = {
+		symbol_in_winbar = {
+			enable = false,
+		},
+		finder = {
+			keys = {
+				toggle_or_open = "<CR>",
+			},
+		},
+		lightbulb = {
+			enable = false,
+			virtual_text = false,
+		},
+	},
+	keys = {
+		{ "<leader>lr", "<Cmd>Lspsaga rename<CR>" },
+		{ "<leader>lc", "<Cmd>Lspsaga code_action<CR>" },
+		{ "<leader>lh", "<Cmd>Lspsaga hover_doc<CR>" },
+	},
+}
