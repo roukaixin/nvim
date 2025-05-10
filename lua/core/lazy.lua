0bce94fc6cadedcbef542c8816e3bfecd7e70ab4
roukaixin <a3427173515@163.com>
2025-05-10 22:46:12 +0800

    chore: 配置 ui

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -1,28 +1,32 @@
 local lazy_path = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazy_path) then
-    local lazy_repo = "https://github.com/folke/lazy.nvim.git"
-    local out = vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "--branch=stable",
-        lazy_repo,
-        lazy_path
-    })
-    if vim.v.shell_error ~= 0 then
-        vim.api.nvim_echo({
-            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-            { out, "WarningMsg" },
-            { "\nPress any key to exit..." },
-        }, true, {})
-        vim.fn.getchar()
-        os.exit(1)
-    end
+	local lazy_repo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"--branch=stable",
+		lazy_repo,
+		lazy_path,
+	})
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazy_path)
 
 require("lazy").setup({
-    spec = {
-        { import = "plugins" }
-    },
+	spec = {
+		{ import = "plugins" },
+	},
+	ui = {
+		border = "rounded",
+	},
 })
+
