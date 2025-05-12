1f722f1ba4217b866b42d491af76719b1559fa70
roukaixin <a3427173515@163.com>
2025-05-12 14:10:37 +0800

    chore: 替换通知

---
+++ lua/plugins/fidget.lua
@@ -0,0 +1,20 @@
+return {
+	"j-hui/fidget.nvim",
+	event = "VeryLazy",
+	opts = {
+		notification = {
+			override_vim_notify = true,
+			window = {
+				align = "top",
+			},
+		},
+	},
+	config = function(_, opts)
+		require("fidget").setup(opts)
+
+		print = function(...)
+			local msg = table.concat(vim.tbl_map(tostring, { ... }), " ")
+			vim.notify(msg, vim.log.levels.INFO)
+		end
+	end,
+}
