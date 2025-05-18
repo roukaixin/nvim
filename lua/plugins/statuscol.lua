54f4e5382378eb0eaf4425ba19d4991dc1d2daa6
roukaixin <a3427173515@163.com>
2025-05-18 23:04:59 +0800

    chore: 去掉 neo-tree 折叠

--- lua/plugins/statuscol.lua
+++ lua/plugins/statuscol.lua
@@ -1,19 +1,43 @@
 return {
 	"luukvbaal/statuscol.nvim",
 	lazy = false,
-	config = function()
+	opts = {
+		ft_ignore = {
+			"neo-tree",
+		},
+	},
+	config = function(_, opts)
 		local builtin = require("statuscol.builtin")
 		require("statuscol").setup({
 			relculright = true,
 			segments = {
-				{ text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+				{
+					text = { builtin.foldfunc },
+					click = "v:lua.ScFa",
+				},
 				{
 					text = { builtin.lnumfunc, " " },
-					condition = { true, builtin.not_empty },
 					click = "v:lua.ScLa",
 				},
-				{ text = { "%s" }, click = "v:lua.ScSa" },
+				{
+					text = { "%s" },
+					click = "v:lua.ScSa",
+				},
 			},
+		})
+		vim.api.nvim_create_autocmd({ "FileType", "BufEnter" }, {
+			callback = function()
+				if vim.tbl_contains(opts.ft_ignore, vim.api.nvim_buf_get_option(0, "filetype")) then
+					vim.api.nvim_cmd({
+						cmd = "setlocal",
+						args = {
+							"foldcolumn=0",
+						},
+					}, {
+						output = false,
+					})
+				end
+			end,
 		})
 	end,
 }
