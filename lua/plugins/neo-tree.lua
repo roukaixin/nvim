7e434769ff0be4d462e234cc727eb5c543c46435
roukaixin <a3427173515@163.com>
2025-05-18 23:10:40 +0800

    chore: 去掉高亮组

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -52,17 +52,6 @@ return {
 	},
 	config = function(_, opts)
 		require("neo-tree").setup(opts)
-		local neotree_highlights = {
-			["NeoTreeGitAdded"] = { fg = "#0EAA00" },
-			["NeoTreeGitConflict"] = { fg = "#FF0000" },
-			["NeoTreeGitDeleted"] = { fg = "#616161" },
-			["NeoTreeGitIgnored"] = { fg = "#717138" },
-			["NeoTreeGitModified"] = { fg = "#0047E4" },
-			["NeoTreeGitUntracked"] = { fg = "#FF6B68" },
-		}
-		for hl_group, hl_config in pairs(neotree_highlights) do
-			vim.api.nvim_set_hl(0, hl_group, hl_config)
-		end
 	end,
 	keys = {
 		{ "<leader>uf", "<Cmd>Neotree toggle<CR>", desc = "切换文件树", silent = true },
