36206da9f86865438993775368483c65fa184313
roukaixin <a3427173515@163.com>
2025-06-06 21:20:35 +0800

    feat: 添加 md 查看文件插件

---
+++ lua/plugins/render-markdown.lua
@@ -0,0 +1,44 @@
+return {
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		ft = {
+			"markdown",
+		},
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"nvim-web-devicons",
+			"HakonHarnes/img-clip.nvim",
+		},
+		opts = {
+			render_modes = true,
+			completions = {
+				blink = {
+					enabled = true,
+				},
+			},
+			sign = {
+				enabled = false,
+			},
+		},
+	},
+	{
+		"HakonHarnes/img-clip.nvim",
+		opts = {
+			default = {
+				dir_path = function()
+					return vim.fn.expand("assets/" .. vim.fn.expand("%:t:r"))
+				end,
+				file_name = "image-%Y%m%d%H%M%S",
+				prompt_for_file_name = false,
+			},
+			filetypes = {
+				markdown = {
+					template = "![$FILE_NAME_NO_EXT]($FILE_PATH)",
+				},
+			},
+		},
+		keys = {
+			{ "<leader>p", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
+		},
+	},
+}
